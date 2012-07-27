7d1b6a327f982fe1a14126333a020d91407d4670
Rick Winfrey <rick.winfrey@gmail.com>
2012-07-27 15:06:37 -0500

    Namespace files
    Signed-off-by: Josh Cheek <josh.cheek@gmail.com>

--- lib/nyan_cat_formatter.rb
+++ lib/nyan_cat_formatter.rb
@@ -2,17 +2,17 @@
 
 rspec_bin = $0.split('/').last
 if rspec_bin == 'spec'
-  ['spec', 'rspec1', 'spec/runner/formatter/base_text_formatter'].each {|f| require f}
+  ['spec', 'nyan_cat_formatter/rspec1', 'spec/runner/formatter/base_text_formatter'].each {|f| require f}
   parent_class = Spec::Runner::Formatter::BaseTextFormatter
   rspec_module = RSpec1
 else
-  ['rspec2','rspec/core/formatters/base_text_formatter'].each {|f| require f}
+  ['nyan_cat_formatter/rspec2', 'rspec/core/formatters/base_text_formatter'].each {|f| require f}
   parent_class = RSpec::Core::Formatters::BaseTextFormatter
   rspec_module = RSpec2
 end
 
 NyanCatFormatter = Class.new(parent_class) do
-  
+
   ESC      = "\e["
   NND      = "#{ESC}0m"
   PASS     = '='
