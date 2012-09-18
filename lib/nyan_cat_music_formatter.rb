68808a9cf2355c3240ee5df605c44d27c3ec02d0
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2012-09-18 16:13:05 -0500

    Added the test and the entry on the README file

--- lib/nyan_cat_music_formatter.rb
+++ lib/nyan_cat_music_formatter.rb
@@ -39,20 +39,20 @@ NyanCatMusicFormatter = Class.new(NyanCatFormatter) do
   def kill_music
     if File.exists? nyan_mp3
       system("killall -9 afplay &>/dev/null") if osx?
-      kill_music_in_linux if linux?
+      kill_music_on_linux if linux?
     end
   end
 
   private
     
   def play_on_linux
-    kernel.system("[ -e #{nyan_mp3} ] && type mpg321 &>/dev/null && mpg321 #{nyan_mp3} &>/dev/null &") if kernel.system('which mpg321 && type mpg321')
-    kernel.system("[ -e #{nyan_mp3} ] && type mpg123 &>/dev/null && mpg123 #{nyan_mp3} &>/dev/null &") if kernel.system('which mpg123 && type mpg123')
+    kernel.system("[ -e #{nyan_mp3} ] && type mpg321 &>/dev/null && mpg321 #{nyan_mp3} &>/dev/null &") if kernel.system('which mpg321 &>/dev/null && type mpg321 &>/dev/null')
+    kernel.system("[ -e #{nyan_mp3} ] && type mpg123 &>/dev/null && mpg123 #{nyan_mp3} &>/dev/null &") if kernel.system('which mpg123 &>/dev/null && type mpg123 &>/dev/null')
   end
 
   def kill_music_on_linux 
-    system("killall -9 mpg321 &>/dev/null") if kernel.system("which mpg321 && type mpg321")
-    system("killall -9 mpg123 &>/dev/null") if kernel.system("which mpg123 && type mpg123")
+    system("killall -9 mpg321 &>/dev/null") if kernel.system("which mpg321 &>/dev/null && type mpg321 &>/dev/null")
+    system("killall -9 mpg123 &>/dev/null") if kernel.system("which mpg123 &>/dev/null && type mpg123 &>/dev/null")
   end
 
 end
